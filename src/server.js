ad701fc6e8f7bf53cfa54d89c3c297e74cb4f349
Luis Fernando <luisfernandobega@gmail.com>
2023-08-16 15:23:09 -0300

    Error handling with class AppError added

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
-const express = require('express')
+require('express-async-errors')
 
+const AppError = require('./utils/AppError')
+
+const express = require('express')
 const routes = require('./routes')
 
 const app = express()
@@ -8,6 +11,21 @@ app.use(express.json())
 
 app.use(routes)
 
+app.use((error, req, res, next) => {
+  if(error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  console.error(error)
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal servor error"
+  })
+})
 
 const PORT = 4444
 app.listen(PORT, () => console.log(`server is running on ${PORT}`))
