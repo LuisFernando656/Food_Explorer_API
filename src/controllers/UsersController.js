a88178fc7fbddf705f6b27c676161293c1bb9b4c
Luis Fernando <luisfernandobega@gmail.com>
2023-08-17 14:52:12 -0300

    Update user function added

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 const AppError = require("../utils/AppError")
 const knex = require('../database')
 
@@ -21,6 +21,52 @@ class UsersController {
     })
 
     res.json()
+  }
+
+  async update (req, res) {
+    const { name, email, password, old_password } = req.body
+    const { id } = req.params
+
+    const user = await knex('users').where('id', id).first()
+
+    if(!user) {
+      throw new AppError('Usuário nao encontrado', 404)
+    }
+
+    const userWithUpdatedEmail = await knex('users')
+      .where('email', email)
+      .whereNot('id', id)
+      .first();
+   
+    if(userWithUpdatedEmail){
+      throw new AppError('Este email já esta em uso')
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if(password && !old_password){
+      throw new AppError('Você precisa informar a senha antiga pra definir uma senha nova')
+    }
+
+    if(password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword){
+        throw new AppError("A senha antiga nao confere")
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await knex('users').where('id', id).update({
+      name,
+      email,
+      password: user.password,
+      updated_at: knex.fn.now()
+    })
+
+    return res.status(200).json()
   }
 }
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,5 +7,6 @@ const usersController = new UsersController()
 const userRouter = Router()
 
 userRouter.post('/', usersController.create)
+userRouter.put('/:id', usersController.update)
 
 module.exports = userRouter
