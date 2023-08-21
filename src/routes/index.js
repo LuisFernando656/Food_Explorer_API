2d56e30f05a08b45f17aa8365907f741153f32a9
Luis Fernando <luisfernandobega@gmail.com>
2023-08-21 11:37:59 -0300

    Sessions controller with routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require('express')
 const usersRoutes = require('./user.routes')
 const dishesRoutes = require('./dishes.routes')
 const ingredientsRoutes = require('./ingredients.routes')
+const sessionsRouter = require('./sessions.routes')
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
 routes.use('/dishes', dishesRoutes)
+routes.use('/sessions', sessionsRouter)
 routes.use('/ingredients', ingredientsRoutes)
 
 module.exports = routes
