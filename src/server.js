e9bd6b3aa2baa55d7d412fb988e6d04251f2ce61
Luis Fernando <luisfernandobega@gmail.com>
2023-09-08 13:31:56 -0300

    axios implemented

--- src/server.js
+++ src/server.js
@@ -5,11 +5,13 @@ const AppError = require('./utils/AppError')
 const express = require('express')
 const routes = require('./routes')
 const uploadConfig = require('./configs/upload')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 
+app.use(cors)
 app.use(routes)
 
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
